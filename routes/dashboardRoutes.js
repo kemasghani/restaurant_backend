acbbd5b6179f828b1563c55acd8b7188a10477d0
Kemas Ghani Sampurna <kemas@treelogy.com>
2025-12-04 20:40:53 +0800

    middleware

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -3,16 +3,16 @@ import {
   getLowStockAlerts, 
   getBranchOrderStats 
 } from "../controllers/dashboardController.js";
-import { verifyToken } from "../middleware/authMiddleware.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // GET: Mengambil daftar bahan baku yang stoknya di bawah batas minimum
 // Endpoint: /api/dashboard/low-stock
-router.get("/low-stock", verifyToken, getLowStockAlerts);
+router.get("/low-stock", authenticate, getLowStockAlerts);
 
 // GET: Mengambil laporan total order per cabang
 // Endpoint: /api/dashboard/branch-reports
-router.get("/branch-reports", verifyToken, getBranchOrderStats);
+router.get("/branch-reports", authenticate, getBranchOrderStats);
 
 export default router;
