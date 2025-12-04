44eeb8028f9ffb7cf6be11d1e746dab311c0b37f
Kemas Ghani Sampurna <kemas@treelogy.com>
2025-12-04 21:57:47 +0800

    update middleware

--- routes/bahanMasukRoutes.js
+++ routes/bahanMasukRoutes.js
@@ -1,7 +1,6 @@
 // routes/orderRoutes.js
 import express from "express";
 import { authenticate } from "../middleware/authMiddleware.js";
-import { authorize } from "../middleware/roleMiddleware.js";
 import {
     createOrder, listOrders, getOrder,
     acceptOrder, rejectOrder, deliverOrder
@@ -12,15 +11,15 @@ const router = express.Router();
 router.use(authenticate);
 
 // cabang buat order
-router.post("/", authorize("cabang"), createOrder);
+router.post("/", createOrder);
 
 // list (supplier see his orders, cabang see their orders)
 router.get("/", listOrders);
 router.get("/:id", getOrder);
 
 // supplier actions
-router.post("/:id/accept", authorize("supplier"), acceptOrder);
-router.post("/:id/reject", authorize("supplier"), rejectOrder);
-router.post("/:id/deliver", authorize("supplier"), deliverOrder);
+router.post("/:id/accept", acceptOrder);
+router.post("/:id/reject", rejectOrder);
+router.post("/:id/deliver", deliverOrder);
 
 export default router;
